0eb642f2b9ec620f3c51bd047e83b622b45b6219
chuday <mikhail_chudaew505@mail.ru>
2023-06-19 08:48:05 +0500

    Create app

---
+++ movieBooking/movieBooking/movieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieBookingApp.swift
+//  movieBooking
+//
+//  Created by Mikhail Chudaev on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct movieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
